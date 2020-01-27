c87f8635547ae7b0767928fe628472f2adc48645
Francisco Campos <franciiscocampos170@gmail.com>
2020-01-27 21:59:59 +0100

    Styling the aplication

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,25 @@
 <template>
   <div id="app">
+      <main>
+        <div class="search-box">
+          <input type="text" class="search-bar" placeholder="Pesquisar..."/>
+        </div>
 
+        <div class="weather-wrap">
+          <div class="location-box">
+            <div class="location">
+              Luanda, Angola
+            </div>
+            <div class="date">
+              Monday 20 January 2020
+            </div>
+          </div>
+          <div class="weather-box">
+            <div class="temp">9॰c</div>
+            <div class="weather">Rain</div>
+          </div>
+        </div>
+      </main>
   </div>
 </template>
 
@@ -9,12 +28,116 @@
 
 export default {
   name: 'App',
+  data() {
+    return  {
+      api_key: '7921ba54500680121d665e9b56a4e0df',
+      url_base: 'https://api.openweathermap.org/data/2.5',
+      query: '',
+      weather:  {
 
+      }
+    },
+    {
+      
+    }
+}
 }
 </script>
 
 <style>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body{
+  font-family: 'montserrat', sans-serif;
+}
 #app {
+  background-image: url('./assets/cold-bg.jpg');
+  background-size: cover;
+  background-position: bottom;
+  transition: 0.4;
+}
 
+main{
+  min-height: 100vh;
+  padding: 25px;
+
+  background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.75)); 
+}
+
+.search-box{
+  width: 100%;
+  margin-bottom: 30px;
+}
+
+.search-box .search-bar{
+  display: block;
+  width: 100%;
+  padding: 15px;
+
+  color: #313131;
+  font-size: 20px;
+
+  appearance: none;
+  border: none;
+  outline: none;
+  background: none;
+
+  background-color: rgba(255, 255, 255, 0.5);
+  border-radius: 0px 16px 0px 16px;
+  transition: 0.4s;
+}
+
+.search-box .search-bar:focus{
+  box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.75);
+  background-color: rgba(255, 255, 255, 0.75);
+  border-radius: 16px 0px 16px 0px;
+}
+.location-box{
+  color: #fff;
+  font-size: 32px;
+  font-weight: 500;
+  text-align: center;
+  text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+}
+.location-box .date{
+  color: #fff;
+  font-size: 20px;
+  font-weight: 300;
+  font-style: italic;
+  text-align: center;
+}
+
+.weather-box{
+  text-align: center;
+}
+
+
+
+.weather-box .temp{
+  display: inline-block;
+  padding: 10px 25px;
+  color: #fff;
+  font-size: 102px;
+  font-weight: 900;
+
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.25);
+  border-radius: 16px;
+  margin: 30px 0px;
+  
+  box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+  }
+
+
+
+.weather-box .weather{
+  color: #fff;
+  font-size: 48px;
+  font-weight: 700;
+  font-style: italic;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
 }
 </style>
